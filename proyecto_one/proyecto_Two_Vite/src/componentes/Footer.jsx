41a8d3b8c64f475f86c1bcf91e4bc391c8fc27ed
SebastianVP123 <sebastianvalenciaperez8@gmail.com>
2025-08-19 15:33:54 -0500

    Eliminar proyecto_Two_Vite duplicado de proyecto_one

--- proyecto_one/proyecto_Two_Vite/src/componentes/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-return (
-<footer style={{ background: '#eee', padding: '1rem', marginTop:
-'2rem' }}>
-<p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
-</footer>
-    );
-};
-export default Footer;
