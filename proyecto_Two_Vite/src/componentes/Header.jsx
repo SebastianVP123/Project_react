c117079773f597afb8522e297ed7e58ca5793313
SebastianVP123 <sebastianvalenciaperez8@gmail.com>
2025-08-19 15:06:07 -0500

    Primer commit

---
+++ proyecto_Two_Vite/src/componentes/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+return (
+<header style={{ background: '#282c34', padding: '1rem', color:
+'white' }}>
+<h1>Mi Sitio Web en React</h1>
+</header>
+);
+};
+export default Header;
---
+++ proyecto_Two_Vite/src/componentes/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Menu from './Menu';
+import Footer from './Footer';
+const Layout = ({ children }) => {
+return (
+<>
+<Header />
+<Menu />
+
+{children}
+
+<Footer />
+</>
+);
+};
+export default Layout;
